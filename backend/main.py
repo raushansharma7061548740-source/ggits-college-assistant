52e29b3d63d2d55b1fe4d2518ff5bcbb41216ebb
raushansharma7061548740-source <raushansharma7061548740@gamil.com>
2026-09-06 19:22:56 +0530

    Fix Render API entry point

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,6 @@ from langgraph.graph import StateGraph, START, END
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 load_dotenv()
